1256e02671dc114eb0f25687f1280bb20d60c1c3
Robin Dupret <robin.dupret@gmail.com>
2017-06-04 18:04:46 +0200

    Some improvements for the user interface
    * Properly display the needed attributes on the admin show page.
    * Make sure that it's possible to edit a user settings without their
      password.
    * Properly allow the `cart_id` and `distribution_point_id` parameters.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,38 @@
 ActiveAdmin.register User do
-  permit_params :first_name, :last_name, :nb_yaourts, :cart, :distribution_point,
-                :email, :password, :password_confirmation
+  permit_params :first_name, :last_name, :nb_yaourts, :cart_id,
+                :distribution_point_id, :email, :password, :password_confirmation
 
   index do
     selectable_column
     column :full_name
     column :nb_yaourts
     column :cart
+    column :distribution_point
     column :email
     actions
   end
 
   filter :first_name
   filter :last_name
+
+  controller do
+    def update_resource(object, attributes)
+      update_method = attributes.first[:password].present? ? :update_attributes : :update_without_password
+      object.send(update_method, *attributes)
+    end
+  end
+
+  show do
+    attributes_table do
+      row :first_name
+      row :last_name
+      row :cart
+      row :email
+      row :admin
+      row :distribution_point
+      row :nb_yaourts
+    end
+  end
 
   form do |f|
     f.inputs "Admin Details" do
